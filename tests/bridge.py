4340e60d130237052bc6170b6ba65196b47bc485
Chris Dent <cdent@matter.lan>
2009-05-27 00:14:37 +0100

    instruments the 'my_thing' bridge class in the bridge subclass

--- tests/bridge.py
+++ tests/bridge.py
@@ -1,4 +1,9 @@
-from testml.bridge import Bridge as SuperBridge
+from testml.bridge import Bridge as SuperBridge, TRANSFORM_CLASSES
+
+TRANSFORM_CLASSES.append('bridge')
+
+def testml_my_thing(self, args):
+    return ' - '.join(self.value.rstrip('\n').split('\n'))
 
 class Bridge(SuperBridge):
     pass
