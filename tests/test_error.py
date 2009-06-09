cea517ca0d7309c8f37f79e14f92208032c3fe39
Ingy döt Net <ingy@ingy.net>
2009-06-08 19:08:12 -0700

    Use grammar submodule.

--- tests/test_error.py
+++ tests/test_error.py
@@ -1,12 +1,8 @@
-import sys
-sys.path.insert(0, 'lib')
+import sys; sys.path.insert(0, 'lib')
+
 from testml.runner.pytest import Runner
 from bridge import Bridge
 
 def test():
-    for test in (Runner(
-        'tests/testml/error.tml',
-        Bridge,
-    ).run()):
-         yield test
+    for test in Runner('tests/testml/error.tml', Bridge).run(): yield test
 
