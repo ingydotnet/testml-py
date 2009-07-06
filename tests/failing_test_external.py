269fe8c704213f331e2afe79fd9663441e7fea90
Ingy döt Net <ingy@ingy.net>
2009-07-06 15:16:46 -0700

    Add a failing test for external data files.

---
+++ tests/failing_test_external.py
@@ -0,0 +1,10 @@
+import sys; sys.path.insert(0, 'lib')
+from testml.runner.pytest import TESTML, test
+TESTML['stream'] = """\
+%TestML: 2.0
+%Plan: 4
+%Data: external2.tml
+%Data: external1.tml
+
+$foo == $bar;
+"""
