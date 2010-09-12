8282626c103c58fa984bc1c4a9f7ab0fc50c172f
Ingy dot Net <ingy@ingy.net>
2010-09-11 21:37:41 -0700

    Generate the testml.grammar module from grammar.

--- src/grammar.py
+++ src/grammar.py
@@ -1,15 +1,36 @@
+#!/usr/bin/env python
+"""
+
+Usage:
+        make_module yaml-grammar-file
+
+"""
+import sys
+import pprint
 import yaml
 
-import sys
+def generate_grammar(yaml_file):
+    grammar = yaml.load(file(sys.argv[1]))
+    data = pprint.pformat(grammar, indent=2)
 
-file = sys.argv[1]
+    print """\
+\"\"\"
+Generated TestML Grammar
+\"\"\"
 
-fh = open(file)
+import pegex.grammar
 
-data = yaml.load(fh)
+class Grammar(pegex.grammar.Grammar):
+    def __init__(self, receiver=None):
+        pegex.grammar.Grammar.__init__(self, receiver=receiver)
+        self.grammar = {}
+        self.grammar.update(
+%(data)s
+)
+""" % locals(),
 
-print """\
-class Grammar(object):
-    def grammar(self):
-        return """,
-print data
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        raise Exception(__doc__)
+    grammar_file = sys.argv[1]
+    generate_grammar(grammar_file)
--- tests/test_import.py
+++ tests/test_import.py
@@ -3,6 +3,7 @@ from package.unittest import *
 class TestImport(TestCase):
     def test_import(self):
         import testml
+        import testml.grammar
 
         self.assertTrue(True, 'testml modules imported cleanly')
 
