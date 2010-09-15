842b1a3efaec66dc881dcf9210c23a93bb914f7f
Ingy dot Net <ingy@ingy.net>
2010-09-15 07:46:18 -0700

    generate test files from a template

--- tests/template.py
+++ tests/template.py
@@ -3,6 +3,6 @@ sys.path.insert(0, '.')
 sys.path.insert(0, 'tests')
 from testml import *
 TestML.runner = 'testml.runtime.unittest'
-TestML.document = 'testml/basic.tml'
+TestML.document = 'testml/[% testml_file %]'
 TestML.bridge = 'bridge'
 test()
